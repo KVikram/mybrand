b0cdc9db18fd6bfe377a597041a1d59391bd22b2
“KVikram” <vikraal10@gmail.com>
2020-06-06 16:23:01 +0100

    code indentation/refactor and renamed ngFor object

--- src/app/home/timeline/timeline.component.ts
+++ src/app/home/timeline/timeline.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+	selector: "app-timeline",
+	templateUrl: "./timeline.component.html",
+	styleUrls: ["./timeline.component.css"],
 })
 export class TimelineComponent implements OnInit {
+	workTimeline: { work: [] };
 
-  workTimeline:any;
-  
-  constructor(private httpClient: HttpClient) { 
-    this.httpClient.get("assets/data/work-timeline.json").subscribe(data =>{
-      this.workTimeline = data;
-      console.log(this.workTimeline);
-    });
-  }
+	constructor(private httpClient: HttpClient) {
+		this.httpClient
+			.get("assets/data/work-timeline.json")
+			.subscribe((data: any) => {
+				this.workTimeline = data;
+			});
+	}
 
-  ngOnInit() {
-    
-  }
-
+	ngOnInit() {}
 }
