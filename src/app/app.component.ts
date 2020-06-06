e7e6449ac82bb985389207f9870c4d0894c047cf
“KVikram” <vikraal10@gmail.com>
2020-06-06 16:19:12 +0100

    added gtag code for google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { filter } from "rxjs/operators";
+import { Router, NavigationEnd } from "@angular/router";
+
+declare var gtag;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'mybrand';
+	title = "mybrand";
+
+	constructor(router: Router) {
+		const navEndEvent$ = router.events.pipe(
+			filter((e) => e instanceof NavigationEnd)
+		);
+		navEndEvent$.subscribe((e: NavigationEnd) => {
+			console.log("gtag invoked");
+			gtag("config", "UA-125406347-2", { page_path: e.urlAfterRedirects });
+		});
+	}
 }
