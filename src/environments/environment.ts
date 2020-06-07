72c40cc9dfdb9827b079b19cbb41e4d5c4232e67
Vikram Kadiam <vikraal10@gmail.com>
2020-06-07 13:27:11 +0100

    added dev label to build number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { version } from '../../package.json';
+import { version } from "../../package.json";
 
 export const environment = {
-  production: false,
-  VERSION: version
+	production: false,
+	VERSION: version + "--dev",
 };
 
 /*
