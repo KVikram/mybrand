c4f0d54f5bdf8f46cfb9f2692591b352483df657
Vikram Kadiam <vikraal10@gmail.com>
2020-06-09 22:48:28 +0100

    added '?' in ngFor object to avoid undefined errors and moved http calls to ngOnInit()

--- src/app/home/timeline/timeline.component.ts
+++ src/app/home/timeline/timeline.component.ts
@@ -7,15 +7,16 @@ import { HttpClient } from "@angular/common/http";
 	styleUrls: ["./timeline.component.css"],
 })
 export class TimelineComponent implements OnInit {
-	workTimeline: { work: [] };
+	workTimeline: any;
 
 	constructor(private httpClient: HttpClient) {
+
+	}
+
+	ngOnInit() {
 		this.httpClient
-			.get("assets/data/work-timeline.json")
-			.subscribe((data: any) => {
+			.get("assets/data/work-timeline.json").subscribe((data: any) => {
 				this.workTimeline = data;
 			});
 	}
-
-	ngOnInit() {}
 }
