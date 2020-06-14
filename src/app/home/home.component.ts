03e5a3bd4baf761481adbf07b76ee8231898b0da
Vikram Kadiam <vikraal10@gmail.com>
2020-06-14 09:19:50 +0100

    removed unused code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,25 +9,12 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-  
-  constructor(private svgIcons:SvgIconService) {
-    //this.svgIcons.init();
-   }
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-}
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
 }
