f5bf77ca9ce7836b56b366973996f7aa7e5f98c2
Vikram Kadiam <vikraal10@gmail.com>
2020-06-15 22:55:30 +0100

    test(e2e): Updated e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should match subtitle', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to mybrand!');
+    expect(page.getTitleText()).toEqual('Technical Architect');
   });
 
   afterEach(async () => {
